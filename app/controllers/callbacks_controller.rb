f47816ac6d448c080146e5997a6bf1944941c360
dicristomanuel <dicristomanuel@icloud.com>
2015-08-15 17:04:24 -0700

    working on getting percentages

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,20 +1,20 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-  
+
   def facebook
-    #check if user has already facebook identity
+    #check if user has already facebook identity connected
     	if session[:facebook] || current_user.identities.any? { |x| x[:provider]["facebook"] }
     		redirect_to root_path
     	else
         session[:facebook] = 'loggedin'
-       #get facebook access_token
+        #get facebook access_token
         fb_response = request.env["omniauth.auth"]
         fb_token = fb_response["credentials"]["token"]
-       #create identity and initialize Koala contant with it
+        #create identity and initialize Koala contant with it
         identity = current_user.identities.create(facebook_categorize(fb_response))
         for_user = identity.fb_authorize(fb_token)
-       #get the last six month's fbposts
+        #get the last six month's fbposts
         posts = get_posts(for_user)
-       #API call post request to bPop_api for fbposts
+        #API call post request to bPop_api for fbposts
         post_fbposts_to_bPop_api(posts, fb_token)
 
   			redirect_to root_path
@@ -44,6 +44,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
     for_user.get_object('me/posts?limit=5000&since=' + since_this_date)
   end
 
+
   def facebook_categorize(fb_auth)
   	return {
   		provider: fb_auth.provider,
@@ -51,8 +52,9 @@ class CallbacksController < Devise::OmniauthCallbacksController
       image_url: fb_auth.info.image,
       profile_url: fb_auth.info.urls.Facebook,
       access_token: fb_auth.credentials["token"]
-    } 
+    }
   end
+
 
   def twitter_categorize(auth)
   	return {
@@ -64,43 +66,51 @@ class CallbacksController < Devise::OmniauthCallbacksController
       statuses_count: auth.extra.raw_info.statuses_count,
       profile_url: auth.info.urls.Twitter,
       access_token: auth.extra.access_token.consumer.key,
-      secret_token: auth.extra.access_token.consumer.secret 
+      secret_token: auth.extra.access_token.consumer.secret
     }
   end
 
+
   def post_fbposts_to_bPop_api(posts, fb_token)
     #create likes / likes_data variables
-    
-    posts.each do |post| 
+    posts.each do |post|
       if post['likes']
         likes = post['likes']['data'].length
         likes_data = post['likes']['data'].to_json
-        
       else
         likes = 0
         likes_data = "0"
       end
+
+      if post['comments']
+        comments = post['comments']['data'].length
+        comments_data = post['comments']['data'].to_json
+      else
+        comments = 0
+        comments_data = "0"
+      end
+
       #define params post request
       params = {
         fbpost: {
-          user_token: current_user.bpop_token,
+          bpop_token: current_user.bpop_token,
           story: post['story'],
           message: post['message'],
           likes: likes,
           likes_data: likes_data,
+          comments_data: comments_data,
           url: post['link'],
           date: post['created_time'][0..9],
           fb_user_token: fb_token
         }
-      } 
-      #send post request
+      }
+      #send post request to bPop_api
       response = Typhoeus::Request.new(
         "http://localhost:4000/fbposts",
         method: :post,
         params: params
       ).run
     end
-    
   end
 
 end
