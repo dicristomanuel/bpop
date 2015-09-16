463acbb6b0c2ebc910daa6d5e5bce42b4c9e47bc
dicristomanuel <dicristomanuel@icloud.com>
2015-09-15 19:23:42 -0700

    carousel working

--- app/assets/javascripts/angular-app/controllers/statsController.js
+++ app/assets/javascripts/angular-app/controllers/statsController.js
@@ -4,26 +4,36 @@ angular.module('bpopApp').controller("statsController",
   $scope.range = 'MONTH';
   $scope.counter = 0;
   $scope.timeRange = "WEEK";
-
+  $scope.subject = 'likes';
   $scope.things = [];
 
+  var div = document.getElementById('div-carousel');
+  var dataToParse = div.getAttribute("data-carousel").replace(/=>/g, ':');
+  dataToParse = JSON.parse(dataToParse);
 
+  $scope.elements = [];
 
-  $scope.elements = [{
-    provider: 'facebook',
-    subject: 'comments',
-    pic: 'https://scontent.xx.fbcdn.net/hphotos-xaf1/v/t1.0-0/s130x130/12004757_621909761284464_4945607141034672307_n.jpg?oh=009a35851674d1cf8c010010086224c4&oe=565F190D',
-    message: 'Nina! Oh, do I miss her too!!',
-    author: 'Alana Sweetwater'
-  },
+  dataToParse.comments.comments.forEach(function(comment) {
+    $scope.elements.push({
+      provider: 'facebook',
+      subject: 'comments',
+      pic: '',
+      message: comment.message,
+      author: comment.user_name
+    });
+  });
 
-  {
-    provider: 'facebook',
-    subject: 'posts',
-    pic: 'https://fbcdn-photos-h-a.akamaihd.net/hphotos-ak-xtf1/v/t1.0-0/s130x130/12009777_622199657922141_1897688646100609121_n.jpg?oh=5fd43e6691c0c4ed95dbf12154eb2740&oe=566C468D&__gda__=1449460131_38a0503049a7e04dedb4b62ca71c8038',
-    message: 'This is the second Post :)',
-    author: 'Olivia'
-  }];
+  dataToParse.posts.posts.forEach(function(post) {
+    $scope.elements.push({
+      provider: 'facebook',
+      subject: 'posts',
+      pic: post.picture,
+      message: post.message,
+      author: '',
+      commentsAmount: post.comments,
+      likesAmount: post.likes
+    });
+  });
 
   var counter = 0;
 
@@ -33,8 +43,8 @@ angular.module('bpopApp').controller("statsController",
     }
 
     $scope.counter = counter;
-    counter++;
-  }, 5000);
+    counter = Math.floor(Math.random() * $scope.elements.length) + 1;
+  }, 4500);
 
 
 }]);
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
 	before_action :user_signed_in?
-	helper_method :get_likes_1W, :get_likes_1M, :get_likes_2M, :get_likes_3M, :get_likes_6M
+	helper_method :get_likes_1W, :get_likes_1M, :get_likes_2M, :get_likes_3M, :get_likes_6M,
+								:get_comments_1W, :get_comments_1M, :get_comments_2M, :get_comments_3M, :get_comments_6M,
+								:get_stats_for_carousel
 
   def index
 
@@ -10,39 +12,215 @@ class HomeController < ApplicationController
 
 	private
 
-	def get_likes_1W
+	# === GET POSTS ===
+
+	def get_posts_1W(limit=nil)
+		if limit
+			request_monthly_posts =	Typhoeus.get(
+				"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=one+week+ago&limit=" + limit.to_s
+			)
+		else
+		request_monthly_posts =	Typhoeus.get(
+			"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=one+week+ago"
+		)
+		end
+		JSON.parse(request_monthly_posts.response_body)['count']
+	end
+
+	def get_posts_1M(limit=nil)
+		if limit
+			request_monthly_posts =	Typhoeus.get(
+				"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=one+month+ago&limit=" + limit.to_s
+			).response_body
+			JSON.parse(request_monthly_posts)
+		else
+		request_monthly_posts =	Typhoeus.get(
+			"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=one+month+ago"
+		)
+		JSON.parse(request_monthly_posts.response_body)['count']
+		end
+	end
+
+	def get_posts_2M(limit=nil)
+		if limit
+			request_monthly_posts =	Typhoeus.get(
+				"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=two+month+ago&limit=" + limit.to_s
+			)
+		else
+		request_monthly_posts =	Typhoeus.get(
+			"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=two+month+ago"
+		)
+		end
+		JSON.parse(request_monthly_posts.response_body)['count']
+	end
+
+	def get_posts_3M(limit=nil)
+		if limit
+			request_monthly_posts =	Typhoeus.get(
+				"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=three+month+ago&limit=" + limit.to_s
+			)
+		else
+		request_monthly_posts =	Typhoeus.get(
+			"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=three+month+ago"
+		)
+		end
+		JSON.parse(request_monthly_posts.response_body)['count']
+	end
+
+	def get_posts_6M(limit=nil)
+		if limit
+			request_monthly_posts =	Typhoeus.get(
+				"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=six+month+ago&limit=" + limit.to_s
+			)
+		else
+		request_monthly_posts =	Typhoeus.get(
+			"http://localhost:4000/fbposts/" + current_user.bpopToken + "?since=six+month+ago"
+		)
+		end
+		JSON.parse(request_monthly_posts.response_body)['count']
+	end
+
+
+	# === GET LIKES ===
+
+	def get_likes_1W(limit=nil)
+		if limit
+			request_monthly_likes =	Typhoeus.get(
+				"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=one+week+ago&limit=" + limit.to_s
+			)
+		else
 		request_monthly_likes =	Typhoeus.get(
 			"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=one+week+ago"
 		)
+		end
 		JSON.parse(request_monthly_likes.response_body)['count']
 	end
 
-	def get_likes_1M
+	def get_likes_1M(limit=nil)
+		if limit
+			request_monthly_likes =	Typhoeus.get(
+				"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=one+month+ago&limit=" + limit.to_s
+			)
+		else
 		request_monthly_likes =	Typhoeus.get(
 			"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=one+month+ago"
 		)
+		end
 		JSON.parse(request_monthly_likes.response_body)['count']
 	end
 
-	def get_likes_2M
+	def get_likes_2M(limit=nil)
+		if limit
+			request_monthly_likes =	Typhoeus.get(
+				"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=two+month+ago&limit=" + limit.to_s
+			)
+		else
 		request_monthly_likes =	Typhoeus.get(
 			"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=two+month+ago"
 		)
+		end
 		JSON.parse(request_monthly_likes.response_body)['count']
 	end
 
-	def get_likes_3M
+	def get_likes_3M(limit=nil)
+		if limit
+			request_monthly_likes =	Typhoeus.get(
+				"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=three+month+ago&limit=" + limit.to_s
+			)
+		else
 		request_monthly_likes =	Typhoeus.get(
 			"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=three+month+ago"
 		)
+		end
 		JSON.parse(request_monthly_likes.response_body)['count']
 	end
 
-	def get_likes_6M
+	def get_likes_6M(limit=nil)
+		if limit
+			request_monthly_likes =	Typhoeus.get(
+				"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=six+month+ago&limit=" + limit.to_s
+			)
+		else
 		request_monthly_likes =	Typhoeus.get(
 			"http://localhost:4000/fblikes/" + current_user.bpopToken + "?since=six+month+ago"
 		)
+		end
 		JSON.parse(request_monthly_likes.response_body)['count']
+	end
+
+
+	# === GET COMMENTS ===
+
+	def get_comments_1W(limit=nil)
+		if limit
+			request_monthly_comments =	Typhoeus.get(
+				"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=one+week+ago&limit=" + limit.to_s
+			)
+		else
+		request_monthly_comments =	Typhoeus.get(
+			"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=one+week+ago"
+		)
+		JSON.parse(request_monthly_comments.response_body)['count']
+		end
+	end
+
+	def get_comments_1M(limit=nil)
+		if limit
+			request_monthly_comments =	Typhoeus.get(
+				"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=one+month+ago&limit=" + limit.to_s
+			).response_body
+			JSON.parse(request_monthly_comments)
+		else
+		request_monthly_comments =	Typhoeus.get(
+			"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=one+month+ago"
+		)
+		JSON.parse(request_monthly_comments.response_body)['count']
+		end
+	end
+
+	def get_comments_2M(limit=nil)
+		if limit
+			request_monthly_comments =	Typhoeus.get(
+				"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=two+month+ago&limit=" + limit.to_s
+			)
+		else
+		request_monthly_comments =	Typhoeus.get(
+			"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=two+month+ago"
+		)
+		JSON.parse(request_monthly_comments.response_body)['count']
+		end
+	end
+
+	def get_comments_3M(limit=nil)
+		if limit
+			request_monthly_comments =	Typhoeus.get(
+				"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=three+month+ago&limit=" + limit.to_s
+			)
+		else
+		request_monthly_comments =	Typhoeus.get(
+			"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=three+month+ago"
+		)
+		JSON.parse(request_monthly_comments.response_body)['count']
+		end
+	end
+
+	def get_comments_6M(limit=nil)
+		if limit
+			request_monthly_comments =	Typhoeus.get(
+				"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=six+month+ago&limit=" + limit.to_s
+			)
+		else
+		request_monthly_comments =	Typhoeus.get(
+			"http://localhost:4000/fbcomments/" + current_user.bpopToken + "?since=six+month+ago"
+		)
+		JSON.parse(request_monthly_comments.response_body)['count']
+		end
+	end
+
+	# ======================
+
+	def get_stats_for_carousel
+		{'comments' => get_comments_1M(10), 'posts' => get_posts_1M(10)}
 	end
 
 
