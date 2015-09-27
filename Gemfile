e7f9a43e61c5b94c8485cfdb4556857717f5170e
dicristomanuel <dicristomanuel@icloud.com>
2015-09-27 12:02:52 -0700

    about to deploy

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
-  gem 'puma-rails'  source 'https://rubygems.org'
 
 
   # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -75,10 +74,10 @@
   group :development, :test do
     # Call 'byebug' anywhere in the code to stop execution and get a debugger console
     gem 'byebug'
-    gem 'puma-rails'
     # Access an IRB console on exception pages or by using <%= console %> in views
     gem 'web-console', '~> 2.0'
 
+    gem 'dotenv-rails'
     # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
     gem 'spring'
 
--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,6 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
 preload_app!
 
@@ -8,11 +9,31 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
-    ActiveRecord::Base.establish_connection(config)
-  end
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
+
+
+# ============================
+# CONFIGURATION FROM TUTORIAL
+# ============================
+# workers Integer(ENV['PUMA_WORKERS'] || 3)
+# threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+#
+# preload_app!
+#
+# rackup      DefaultRackup
+# port        ENV['PORT']     || 3000
+# environment ENV['RACK_ENV'] || 'development'
+#
+# on_worker_boot do
+#   # worker specific setup
+#   ActiveSupport.on_load(:active_record) do
+#     config = ActiveRecord::Base.configurations[Rails.env] ||
+#                 Rails.application.config.database_configuration[Rails.env]
+#     config['pool'] = ENV['MAX_THREADS'] || 16
+#     ActiveRecord::Base.establish_connection(config)
+#   end
+# end
+#
