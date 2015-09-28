fe4dd2b34c543e147bac96c9a8b1301f097775f4
dicristomanuel <dicristomanuel@icloud.com>
2015-09-28 16:25:44 -0700

    modified internal links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,5 @@
 
 //= require angular-app/app
 //= require_tree ./templates
-//= require_tree ./angular-app/modules
-//= require_tree ./angular-app/filters
-//= require_tree ./angular-app/directives
-//= require_tree ./angular-app/models
-//= require_tree ./angular-app/services
 //= require_tree ./angular-app/controllers
 //= require main
